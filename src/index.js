ced9728402f1dd266eaade023c2a76f275785140
Vu Viet Duy <vuvietduy1234@gmail.com>
2023-06-14 09:04:44 +0700

    Jun 14, 2023

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Config template engine handlebars
+const hbs = exphbs.create({
+  extname: '.hbs'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
 
 app.get('/', (req, res) => {
-  return res.send('Hello World!')
+  res.render('home');
 })
 
-//127.0.0.1 - localhost
+app.get('/news', (req, res) => {
+  res.render('news');
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  // console.log(`Example app listening on port ${port}`)
 })

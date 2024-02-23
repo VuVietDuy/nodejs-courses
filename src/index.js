bb7eb02bfbfcfeff395471860cbe7879187dea37
VuVietDuy <vuvietduy1234@gmail.com>
2024-02-23 15:44:06 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 const route = require('./routes');
 const db = require('./config/db');
@@ -32,5 +32,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () => {
-  // console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
